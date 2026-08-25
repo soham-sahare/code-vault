5be9ae9fc59da7bc309c244c4cc0cfbc9ead6859
sohamsahare <sohamsahare123@gmail.com>
2026-08-25 15:52:01 +0530

    refactor(auth): map session user name directly to username

--- auth.ts
+++ auth.ts
@@ -50,7 +50,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
           return {
             id: user.id,
             email: user.email,
-            name: user.name,
+            name: user.username || user.email,
           };
         }
 
